af0d886f684e89b26b6493184608e279606eea4f
alan <swar.jan21@gmail.com>
2023-01-13 01:13:23 +0300

    try to fix some bug in db

---
+++ app/Models/Lable.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Lable extends Model
+{
+    use HasFactory;
+    protected $fillabel = [
+        'title',
+     ];
+
+     
+     public function task()
+     {
+         return $this->belongsTo(Task::class);
+     }
+}
