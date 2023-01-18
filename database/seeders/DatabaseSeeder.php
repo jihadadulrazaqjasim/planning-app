7a986ca302891149581ab20c0a7e963c52df287e
alan <swar.jan21@gmail.com>
2023-01-18 20:26:36 +0300

    add postman file in the root

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,22 +14,17 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-<<<<<<< HEAD
-        // \App\Models\User::factory(10)->create();
-
-=======
         // \App\Models\Tester::factory(10)->create();
         \App\Models\User::factory(10)->create();
         \App\Models\Board::factory(10)->create();
         \App\Models\Task::factory(10)->create();
-        \App\Models\Lable::factory(10)->create();
+        \App\Models\Label::factory(10)->create();
         \App\Models\Status::factory(10)->create();
         // \App\Models\User::factory(10)->create();
 
         // \App\Models\Developer::factory(10)->create();
         // \App\Models\Owner::factory(10)->create();
         
->>>>>>> 2d543dc75c5dc73c9fb085a26b5bc57c494fbbf1
         // \App\Models\User::factory()->create([
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
