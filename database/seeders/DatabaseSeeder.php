1b2d2c00c08c375d5e520e9a78701972b506f324
alan <swar.jan21@gmail.com>
2023-01-18 20:20:54 +0300

    comit

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -18,7 +18,7 @@ class DatabaseSeeder extends Seeder
         \App\Models\User::factory(10)->create();
         \App\Models\Board::factory(10)->create();
         \App\Models\Task::factory(10)->create();
-        \App\Models\Lable::factory(10)->create();
+        \App\Models\Label::factory(10)->create();
         \App\Models\Status::factory(10)->create();
         // \App\Models\User::factory(10)->create();
 
